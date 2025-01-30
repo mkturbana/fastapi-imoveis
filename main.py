8dc90d1cd63733db99149ac1b0e43729344f576f
mkturbana <mkturbana@gmail.com>
2025-01-30 17:43:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -98,14 +98,11 @@ async def fetch_property_info(property_code: str):
 # 🏡 Função interna para buscar HTML com Selenium (agora chamada dentro de /extract-code/)
 
 def fetch_html_with_selenium(url: str) -> str:
-    options = Options()
+    options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option("useAutomationExtension", False)
-
+   
     # Adiciona User-Agent realista
     options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
