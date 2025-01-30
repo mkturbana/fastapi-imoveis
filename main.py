21d9dace10512556bea2375ee245df0b7daa6615
mkturbana <mkturbana@gmail.com>
2025-01-30 17:33:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -111,7 +111,8 @@ def fetch_html_with_selenium(url: str) -> str:
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
     )
 
-    service = ChromeService(ChromeDriverManager().install())
+    CHROME_DRIVER_PATH = "/usr/local/bin/chromedriver"
+    service = ChromeService(CHROME_DRIVER_PATH)
     driver = webdriver.Chrome(service=service, options=options)
 
     # Define tempo máximo de carregamento
