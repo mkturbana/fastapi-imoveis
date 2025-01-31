c94b56efe9a7f9d8ba2b5b798a6a6d335cbe908c
mkturbana <mkturbana@gmail.com>
2025-01-31 17:16:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -231,7 +231,7 @@ async def fetch_property_info(property_code: str):
 async def fetch_html_with_playwright(url: str) -> str:
     try:
         async with async_playwright() as p:
-            browser = await p.chromium.launch(headless=False)  # Abrir navegador visível para debug
+            browser = await p.chromium.launch(headless=True)  # Abrir navegador visível para debug
             context = await browser.new_context(
                 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
             )
