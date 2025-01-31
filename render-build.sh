7b811dee72991cf2a4dd24957ac4b009fe284095
mkturbana <mkturbana@gmail.com>
2025-01-31 10:53:36 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
+set -e  # Faz o script parar se algum comando falhar
 
 echo "🚀 Instalando dependências do Python..."
 pip install -r requirements.txt
 
-echo "🔧 Instalando Playwright..."
+echo "🛠 Instalando Playwright..."
 pip install playwright
 
 echo "🌍 Baixando navegadores Playwright..."
-playwright install chromium
 playwright install --with-deps
-
 
 echo "✅ Configuração concluída!"
