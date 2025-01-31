d6f73a6841d29ee6949e245c0a35ed813682b35e
mkturbana <mkturbana@gmail.com>
2025-01-31 11:55:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
-@app.get("/", include_in_schema=False, methods=["GET", "HEAD"])
+@app.route("/", methods=["GET", "HEAD"], include_in_schema=False)
 async def root():
     return RedirectResponse(url="/docs")
 
